f2d287541a9d5ea88d9218733eb28dd795b2634d
RadRussianRus <radrussianrus@gmail.com>
2020-06-08 02:42:09 +0300

    Fix italic with bold semibold

--- ui/style/style_core_font.cpp
+++ ui/style/style_core_font.cpp
@@ -412,10 +412,12 @@ FontData::FontData(int size, uint32 flags, int family, Font *other)
 #endif // !DESKTOP_APP_USE_PACKAGED_FONTS
 		}
 
-		if (_flags & FontItalic) {
-			f.setStyleName("Semibold Italic");
-		} else {
-			f.setStyleName("Semibold");
+		if (!CustomSemiboldIsBold) {
+			if (_flags & FontItalic) {
+				f.setStyleName("Semibold Italic");
+			} else {
+				f.setStyleName("Semibold");
+			}
 		}
 	}
 
